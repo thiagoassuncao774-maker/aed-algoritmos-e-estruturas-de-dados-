84644a8e4d86f500e56efcf36869c78859330bf4
thiagoassuncao774-maker <thiagoassuncao774@gmail.com>
2026-03-24 20:30:31 -0300

    Add files via upload
    exercicio aula 1 matriz

---
+++ conteudomatriz1.py
@@ -0,0 +1,18 @@
+matriz=[
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [7, 8, 9],
+    ]
+
+for i in range(len(matriz)):
+   # print ([i])
+    for n in range(len(matriz[i])):
+   # print ([n])
+            print ([matriz],[i])
+            print ("teste")
+            print ([matriz],[n])
+         #print (linhas)
+#for n in range((len)matriz[n]):
+    #for i in range((len)matriz[i]):
+        #print (matriz[n][i])
